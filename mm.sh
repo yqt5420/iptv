7c78a9a2b21dced94a331aaf8bf4ba7b2cdeb6d8
yqt5420 <54099324+yqt5420@users.noreply.github.com>
2025-11-26 10:45:48 +0800

    Update mm.sh

--- mm.sh
+++ mm.sh
@@ -304,7 +304,7 @@ download_mihomo() {
 
 # 创建systemd服务文件
 create_systemd_service() {
-    cat > /etc/systemd/system/mihomo.service << 'EOF'
+    cat > /etc/systemd/system/mihomo.service << EOF
 [Unit]
 Description=mihomo transparent proxy service
 After=network.target
